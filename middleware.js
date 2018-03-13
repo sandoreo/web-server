5544e35966d93844f0b4f3a91fc25480fde6d243
dg.lee <dg.lee@realonsoft.com>
2018-03-13 11:04:15 +0900

    Refactor middleware

---
+++ middleware.js
@@ -0,0 +1,11 @@
+// Use module exports to expose middleware
+module.exports = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, res, next){
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
